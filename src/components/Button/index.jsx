a45d0e1aebaad899b1b4a5bb2630115e037e40dd
withoutspaces <dms@ic.ufal.br>
2022-02-12 15:44:03 -0300

    updating button props

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,7 +3,13 @@ import './styles.css'
 export default function Button(props) {
     return(
         <div className="container-button">
-            <input type="button" value={props.value} className="button" onClick={props.function}/>
+            <input 
+                onClick={props.function}
+                value={props.value}
+                key={props.key} 
+                type="button" 
+                className="button"
+            />
         </div>
     )
 }
