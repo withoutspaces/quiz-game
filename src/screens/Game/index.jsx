c44b6bb40402498418d415c9f8c20898920c5ce6
withoutspaces <dms@ic.ufal.br>
2022-02-12 02:44:49 -0300

    adding components to screen

--- src/screens/Game/index.jsx
+++ src/screens/Game/index.jsx
@@ -1,22 +1,49 @@
+import { useState, useEffect } from "react";
+import Button from "../../components/Button";
+import Title from "../../components/Title";
+import Logo from  '../../assets/logo2.png'
 import api from "../../services/api";
-import { useState } from "react";
+
+import './styles.css'
 
 export default function GameScreen() {
     const [questions, setQuestions] = useState({})
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+        getQuestions()
+    }, [])
 
     function getQuestions() {
         api
-        .get('amount=10&category=9&difficulty=easy')
+        .get('?amount=1&category=9&difficulty=easy&type=multiple')
         .then((response) => {
             setQuestions(response.data)
+            setLoaded(true)
             console.log(questions)
             })
         .catch((err) => alert('opss...' + err))
     }
 
     return(
-        <div>
-            <h1>teste</h1>
-        </div>
+        <section className="container">
+            <div className="card-game">
+                <img src={Logo} alt="logo data app" className="logo2" />
+                {loaded ?
+                    <div className="quiz-box">
+                        <Title  
+                            question_number={1}
+                            title='Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."'
+                        />         
+                        <Button value='Lorem ipsum dolor'/>
+                        <Button value='Lorem ipsum dolor'/>
+                        <Button value='Lorem ipsum dolor'/>
+                        <Button value='Lorem ipsum dolor'/>
+                    </div>
+                :
+                    <p>carregando...</p>
+            }
+            </div>
+        </section>
     );
 }
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,5 +1,5 @@
 import './style.css'
-import Logo from '../../assets/logo.jpg'
+import Logo from '../../assets/logo.png'
 
 import { Link } from 'react-router-dom';
 
@@ -8,14 +8,14 @@ export default function Home() {
         <div className='container'>
             <div className="card">
                 <img src={Logo} alt="Logo show do milhão" className='logo'/>
-                <div>
-                    <h3 className='title'>Ma-Maoe, bem-vindo ao <br/>Show do Milhão</h3>
-                </div>
-                <input type="text" name="userName" className="userName" placeholder='Digite seu nome' />
-                <Link to='/quiz'  className='btn-container' >
-                    <input type="button" value="Iniciar" className='initButton'/>
-                </Link>
                 
+                <div className='info-box'>
+                    <p className='title'>Olá, seja bem-vindo ao Data Quiz! <br/> Para iniciarmos, digite seu nome</p>
+                    <input type="text" name="userName" className="userName" placeholder='Digite seu nome' />
+                    <Link to='/quiz'  className='btn-container' >
+                        <input type="button" value="Começar" className='initButton'/>
+                    </Link> 
+                </div>
             </div>
         </div>
     );
