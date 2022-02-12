3dc9a4a97d8b3ced149d07dd05eae2f4c1c03080
withoutspaces <dms@ic.ufal.br>
2022-02-12 15:44:54 -0300

    api now shuffle data

--- src/screens/Game/index.jsx
+++ src/screens/Game/index.jsx
@@ -5,40 +5,62 @@ import Logo from  '../../assets/logo2.png'
 import api from "../../services/api";
 
 import './styles.css'
+import ConfirmButton from "../../components/ConfirmButton";
 
 export default function GameScreen() {
     const [questions, setQuestions] = useState({})
+    const [alternatives, setAlternatives] = useState([])
     const [loaded, setLoaded] = useState(false)
 
-    useEffect(() => {
-        getQuestions()
+
+    useEffect(() =>{
+        getQuestions();
     }, [])
 
-    function getQuestions() {
-        api
-        .get('?amount=1&category=9&difficulty=easy&type=multiple')
-        .then((response) => {
-            setQuestions(response.data)
-            setLoaded(true)
-            console.log(questions)
-            })
-        .catch((err) => alert('opss...' + err))
+ 
+    async function getQuestions() {
+        const response = await api.get('?amount=1&difficulty=easy&type=multiple')
+        const answer = response.data.results
+        const ans = answer.map(result => [...result.incorrect_answers, result.correct_answer])
+
+        const shuffle = (arr) => {
+            for (let i = arr.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * i)
+                const temp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = temp
+            }
+            return arr;
+        };
+
+        const shuffledAnswers = ans.map(questionsAns => shuffle(questionsAns))
+
+        console.log(shuffledAnswers, answer)
+        setAlternatives(shuffledAnswers);
+        setQuestions(answer)
+        setLoaded(true)
     }
 
     return(
         <section className="container">
-            <div className="card-game">
+            <div className="card-game">          
                 <img src={Logo} alt="logo data app" className="logo2" />
+
                 {loaded ?
                     <div className="quiz-box">
                         <Title  
                             question_number={1}
-                            title='Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."'
-                        />         
-                        <Button value='Lorem ipsum dolor'/>
-                        <Button value='Lorem ipsum dolor'/>
-                        <Button value='Lorem ipsum dolor'/>
-                        <Button value='Lorem ipsum dolor'/>
+                            title={questions[0].question}
+                        />       
+                        {alternatives[0].map((res) => {
+                            console.log(alternatives)
+                            return(
+                                <Button 
+                                    value={res}
+                                />
+                            )
+                        })}
+                        <ConfirmButton />
                     </div>
                 :
                     <p>carregando...</p>
