958be1fb632900d91d3425d41be34fc237eb734c
withoutspaces <dms@ic.ufal.br>
2022-02-13 17:14:59 -0300

    fixing html especial characters to react

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,18 @@
 import './styles.css'
 
 export default function Button(props) {
+
+    const decodeString = (value) => {
+        const txt = document.createElement('textarea')
+        txt.innerHTML = value
+        return txt.value
+    }
+
     return(
         <div className="container-button">
             <input 
                 onClick={props.function}
-                value={props.value}
+                value={decodeString(props.value)}
                 key={props.key} 
                 type="button" 
                 className="button"
--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,10 +1,17 @@
 import './styles.css'
 
 export default function Title(props) {
+
+    const decodeString = (value) => {
+        const txt = document.createElement('textarea')
+        txt.innerHTML = value
+        return txt.value
+    }
+
     return(
         <div className="container-title">
             <h4>Question {props.question_number}</h4>
-            <p>{props.title}</p> 
+            <p>{decodeString(props.title)}</p> 
         </div>
     )
 }
